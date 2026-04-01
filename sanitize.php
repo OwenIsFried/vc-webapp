60e141a043ed652dcf31deeee8b2caaa3d3c4ade
OwenIsFried <stormfry113@gmail.com>
2026-03-31 20:55:39 -0400

    Implemented soft-coded sanitizations.

--- sanitize.php
+++ sanitize.php
@@ -1,7 +1,34 @@
 <?php
 
-function sanitize($string){
-    
-    return preg_replace("/[\t\n\r=\"%;*<>$\/]/i", "", $string);
+function sanitizeCity($city) {
+    $city = preg_replace("/[^a-zA-Z\s\-]/", "", $city);
+    $city = trim($city);
+    if ($city === "") {
+        $city = "New York";
+    }
+    return $city;
+}
 
+function sanitizeUnit($unit) {
+    return ($unit === 'metric') ? 'metric' : 'imperial';
+}
+
+function safeFileGetContents($url) {
+    $parsed = parse_url($url);
+    if (!isset($parsed['host']) || $parsed['host'] !== 'api.openweathermap.org') {
+        return false;
+    }
+    return @file_get_contents($url);
+}
+
+function safeFloat($value) {
+    return isset($value) ? floatval($value) : 0;
+}
+
+function safeInt($value) {
+    return isset($value) ? intval($value) : 0;
+}
+
+function safeString($value) {
+    return isset($value) ? htmlspecialchars($value) : '';
 }
